2a8ac648f51f6b7c0e654e64ce85f8e93eaa304c
jinhwansuh <nick02190219@gmail.com>
2022-07-23 15:21:58 +0900

    jest mock 사용해보기

--- src/components/Login.test.tsx
+++ src/components/Login.test.tsx
@@ -112,4 +112,20 @@ describe('when button click', () => {
 
     await waitFor(() => expect(buttonEl).not.toHaveTextContent(/wait/i));
   });
+
+  test('username should be rendered after fetching', async () => {
+    render(<Login />);
+    const buttonEl = screen.getByRole('button');
+    const usernameInputEl = screen.getByPlaceholderText(/username/i);
+    const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+    const testValue = 'test';
+    fireEvent.change(usernameInputEl, { target: { value: testValue } });
+    fireEvent.change(passwordInputEl, { target: { value: testValue } });
+    fireEvent.click(buttonEl);
+
+    const userItem = await screen.findByText('John');
+
+    expect(userItem).toBeInTheDocument();
+  });
 });
